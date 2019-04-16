28b0d99cee345f473ecb76be9fd50355e52a4c70
kiwon kim <kiwonkim0307@gmail.com>
2019-04-16 23:35:31 +0900

    servlet ex06 공부

---
+++ src/main/java/Study/ex06/Servlet02.java
@@ -0,0 +1,24 @@
+package Study.ex06;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+public class Servlet02 extends HttpServlet{
+
+	
+	@Override
+	public void init() throws ServletException {
+		System.out.println("ex06/s2 init()");
+	}
+	
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		System.out.println("/ex06/s2 service()");
+	}
+}
